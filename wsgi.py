210b1707ae3e200cc21af71cb2be66250f3f8c7a
karim-v <villalbaacevalk@gmail.com>
2026-03-26 01:34:32 +0100

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for miweb project.
+WSGI config for mi_web project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'miweb.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mi_web.settings')
 
 application = get_wsgi_application()
